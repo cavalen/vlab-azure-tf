2779e45b39f15cd984a3ae6b062daba06845f10d
cavalen <cavalen@hotmail.com>
2020-11-19 22:03:05 -0500

    testing ...

--- bigip.tf
+++ bigip.tf
@@ -221,9 +221,7 @@ resource "azurerm_virtual_machine_extension" "f5vm01-run-startup-cmd" {
         "fileUris": [
             "https://raw.githubusercontent.com/cavalen/vlab-azure-tf/master/files/runtime-init-conf-param.yaml"
         ],
-        "commandToExecute": "mkdir -p /config/cloud; mkdir -p /var/log/cloud/azure; cp runtime-init-conf-param.yaml /config/cloud/runtime-init-conf.yaml",
-        "commandToExecute": "sed -i -- 's/VIPVALUE/${var.f5publicvip}/g' /config/cloud/runtime-init-conf.yaml; sed -i -- 's/NODEVALUE/${var.serverip}/g' /config/cloud/runtime-init-conf.yaml; sed -i -- 's/SELFIPVALUE/${var.f5vm01ext}/g' /config/cloud/runtime-init-conf.yaml",
-        "commandToExecure": "curl -L https://cdn.f5.com/product/cloudsolutions/f5-bigip-runtime-init/v1.0.0/dist/f5-bigip-runtime-init-1.0.0-1.gz.run -o f5-bigip-runtime-init-1.0.0-1.gz.run && bash f5-bigip-runtime-init-1.0.0-1.gz.run -- '--cloud azure' 2>&1; f5-bigip-runtime-init --config-file /config/cloud/runtime-init-conf.yaml 2>&1"
+        "commandToExecute": "mkdir -p /config/cloud; mkdir -p /var/log/cloud/azure; cp runtime-init-conf-param.yaml /config/cloud/runtime-init-conf.yaml; sed -i -- 's/VIPVALUE/${var.f5publicvip}/g' /config/cloud/runtime-init-conf.yaml; sed -i -- 's/NODEVALUE/${var.serverip}/g' /config/cloud/runtime-init-conf.yaml; sed -i -- 's/SELFIPVALUE/${var.f5vm01ext}/g' /config/cloud/runtime-init-conf.yaml; curl -L https://cdn.f5.com/product/cloudsolutions/f5-bigip-runtime-init/v1.0.0/dist/f5-bigip-runtime-init-1.0.0-1.gz.run -o f5-bigip-runtime-init-1.0.0-1.gz.run && bash f5-bigip-runtime-init-1.0.0-1.gz.run -- '--cloud azure' 2>&1; f5-bigip-runtime-init --config-file /config/cloud/runtime-init-conf.yaml 2>&1"
         }
     SETTINGS
 
